7acb03a5d56563ca1d0802427002d211b5c1f2ee
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2019-08-10 17:43:13 -0300

    Troca de cor de fundo no darkmode

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 $(function(){
 
+    function darkmodeActivate() {
+        $('body').css({
+            backgroundColor: '#2B2B2B'
+        })
+    }
+
     $('#darkmode').click(function(){
         if ($(this).attr('src' ) == 'assets/img/firefly-dark.png') {
             $(this).attr('src', 'assets/img/firefly.png');
+            darkmodeActivate();
         } else if ($(this).attr('src') == 'assets/img/firefly.png') {
             $(this).attr('src', 'assets/img/firefly-dark.png');
         }
