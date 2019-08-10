7866feee3de750c0fad24e7bc5a21d5b12986192
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2019-08-10 20:51:58 -0300

    Adicao da estrutura da pagina do jogo

--- game.php
+++ game.php
@@ -1,12 +1,12 @@
 <?php
 
-    session_start();
+    // session_start();
 
-    if (!isset($_SESSION['dataPlayer'])) {
-        header('location: index.php');
-    }
+    // if (!isset($_SESSION['dataPlayer'])) {
+    //     header('location: index.php');
+    // }
     
-    session_destroy();
+    // session_destroy();
 ?>
 
 <!DOCTYPE html>
@@ -15,9 +15,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>SmashBug</title>
 </head>
 <body>
-    
+    <div class="container-game">
+        <div class="info row">
+            <div class="time col-6">
+                <p>Tempo</p>
+                <span>0:32</span>
+            </div>
+            <div class="score col-6">
+                <p>Pontuação</p>
+                <span>2</span>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
